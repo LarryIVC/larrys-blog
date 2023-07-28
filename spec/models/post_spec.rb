29f2d346737d334c89385e602a81e40d2870a2c9
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-28 18:42:07 -0400

    Add test for all methods in the models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,42 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  person1 = User.new(name: 'Tom', photo: 'https://unplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-  subject { Post.new(author_id: person1, title: 'My first post', text: 'This is my first post.') }
+  person1 = User.create(name: 'Tom',
+                        photo: 'https://unplash.com/photos/F_-0BxGuVvo',
+                        bio: 'Teacher from Mexico.',
+                        post_counter: 0)
+  subject do
+    Post.new(author_id: person1,
+             title: 'My first post',
+             text: 'This is my first post.',
+             comments_counter: 0,
+             likes_counter: 0)
+  end
   before { subject.save }
 
   it 'Title must not be blank' do
+    subject.author = person1
     subject.title = nil
     expect(subject).to_not be_valid
+
+    subject.title = 'Tom'
+    expect(subject).to be_valid
   end
 
   it 'Title must be present' do
+    subject.author = person1
     subject.title = nil
     expect(subject).to_not be_valid
+
+    subject.title = 'Tom'
+    expect(subject).to be_valid
   end
 
   it 'title must be less than or equal to 250 characters' do
+    subject.author = person1
     subject.title = 'a' * 251
     expect(subject).to_not be_valid
+
+    subject.title = 'a' * 100
+    expect(subject).to be_valid
   end
 
   it 'comments_counter must be integer' do
+    subject.author = person1
     subject.comments_counter = 'a'
     expect(subject).to_not be_valid
+
+    subject.comments_counter = 1
+    expect(subject).to be_valid
   end
 
   it 'comments_counter must be greater than or equal to 0' do
+    subject.author = person1
     subject.comments_counter = -1
     expect(subject).to_not be_valid
+
+    subject.comments_counter = 1
+    expect(subject).to be_valid
   end
 
   it 'likes_counter must be integer' do
+    subject.author = person1
     subject.likes_counter = 'a'
     expect(subject).to_not be_valid
+
+    subject.likes_counter = 1
+    expect(subject).to be_valid
   end
 
   it 'likes_counter must be greater than or equal to 0' do
+    subject.author = person1
     subject.likes_counter = -1
     expect(subject).to_not be_valid
+
+    subject.likes_counter = 1
+    expect(subject).to be_valid
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  subject { User.new(name: 'Tom', photo: 'https://unplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 0) }
   before { subject.save }
 
   it 'Name must not be blank' do
     subject.name = nil
     expect(subject).to_not be_valid
+
+    subject.name = 'Tom'
+    expect(subject).to be_valid
   end
 
   it 'Name must be present' do
     subject.name = nil
     expect(subject).to_not be_valid
+
+    subject.name = 'Tom'
+    expect(subject).to be_valid
   end
 
   it 'post_counter must be integer' do
     subject.post_counter = 'a'
     expect(subject).to_not be_valid
+
+    subject.post_counter = 4
+    expect(subject).to be_valid
   end
 
   it 'post_counter must be greater than or equal to 0' do
     subject.post_counter = -1
     expect(subject).to_not be_valid
+
+    subject.post_counter = 4
+    expect(subject).to be_valid
+  end
+
+  it '#return_three_recent_posts' do
+    number = User.return_three_recent_posts(subject.id).length
+    expect(number).to be <= 3
   end
 end
