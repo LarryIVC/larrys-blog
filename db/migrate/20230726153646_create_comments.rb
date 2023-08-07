29a6b68081d09cf80c7a1e05826444fa51d769b0
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-07 11:02:25 -0400

    Fix code reviewer bugs

--- db/migrate/20230726153646_create_comments.rb
+++ db/migrate/20230726153646_create_comments.rb
@@ -1,14 +1,14 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :auhtorId
-      t.integer :PostId
+      t.integer :author_id
+      t.integer :post_id
       t.string :Text
 
       t.timestamps
     end
 
-    # add_foreign_key :comments, :users, column: :authorId
-    # add_foreign_key :comments, :posts, column: :PostId
+    add_foreign_key :comments, :users, column: :author_id
+    add_foreign_key :comments, :posts, column: :post_id
   end
 end
