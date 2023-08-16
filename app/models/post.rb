1cfec376e56d1358dc26a9762851565b284ee0ca
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-08-16 16:01:30 -0400

    Updating model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,29 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
+
+  has_many :comments do
+    def with_author_name
+      select('comments.*, users.name as author_name')
+        .joins(:author)
+    end
+  end
+
   has_many :likes
 
   # after_save :update_comments_counter, :update_likes_counter
   after_create :update_author_posts_counter
+
+  def self.post_comments
+    left_outer_joins(:comments)
+      .select('posts.id,
+          posts.title,
+          posts.text as post_text,
+          posts.comments_counter,
+          posts.likes_counter,
+          comments.text')
+      .order('posts.created_at DESC')
+  end
 
   def return_five_recent_comments(post_id)
     post1 = Post.find(post_id)
